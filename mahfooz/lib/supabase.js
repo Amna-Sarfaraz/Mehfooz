c774a119cda17c74f41e4ff722a9fcc46241f67b
IQRAAMIR1 <iqraamir961@gmail.com>
2026-05-02 07:52:04 +0500

    Update supabase.js

--- mahfooz/lib/supabase.js
+++ mahfooz/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://pwkpsyfgiracqhsdvrbf.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB3a3BzeWZnaXJhY3Foc2R2cmJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc2MTQ1ODcsImV4cCI6MjA5MzE5MDU4N30.Th-8bek4uKmM1fE5xsTWlsmJEfJWhgNKm0CFpTb5Kmg'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
